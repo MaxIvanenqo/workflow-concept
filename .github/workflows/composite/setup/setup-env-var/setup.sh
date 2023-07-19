d9640bd57edfebc8e28499dcad95347e2d7d707a
Max Ivanenqo <max.ivanenqo@gmail.com>
2023-07-20 00:02:18 +0200

    iterate

--- .github/workflows/composite/setup/setup-env-var/setup.sh
+++ .github/workflows/composite/setup/setup-env-var/setup.sh
@@ -4,7 +4,6 @@ JAVA_DISTRIBUTION="zulu"
 MAVEN_GROUP_ID="com.example.project"
 MAVEN_OPTS="-DgenerateBackupPoms=false -Dsurefire.skipTests=false"
 MAVEN_RESOLVE_RANGES_OPTS="-DallowMajorUpdates=false -DallowMinorUpdates=false -DallowIncrementalUpdates=true -DallowSnapshots=false -DgenerateBackupPoms=false"
-MAVEN_SET_VERSION_OPTS="-DremoveSnapshot=true -DprocessParent=true -DprocessAllModules=true -DprocessDependencies=true -DprocessPlugins=false -DgenerateBackupPoms=false -DgroupId=com.example.project -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}"
 
 MAVEN_PROFILE_NAME=$1
 SERVER_USERNAME=$2
